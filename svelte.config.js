19f36f9d79a54c01c0b41d219157746f877f3331
jeffreycaine <me@jeffwilliams.dev>
2022-03-17 12:09:25 -0600

    wrench: change to azure swa adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: azure(),
 		prerender: {
 			default: true,
 		}
